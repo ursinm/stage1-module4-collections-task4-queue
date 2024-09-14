eb1d3e4a405ef5d79f7eebc212a8da69b2279a41
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:55:11 +0300

    Update DishOrderDeterminerTest.java

--- src/test/java/com/epam/collections/queue/DishOrderDeterminerTest.java
+++ src/test/java/com/epam/collections/queue/DishOrderDeterminerTest.java
@@ -10,17 +10,38 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DishOrderDeterminerTest {
+
     public static Stream<Arguments> testCases() {
         return Stream.of(
+                // Без блюд (граничный случай)
                 Arguments.of("WithNoDishes",
                         0,
                         3,
                         List.of()),
 
+                // Стандартное количество блюд, пропуская каждое 4-е блюдо
                 Arguments.of("WithDishes",
                         11,
                         4,
-                        List.of(4, 8, 1, 6, 11, 7, 3, 2, 5, 10, 9))
+                        List.of(4, 8, 1, 6, 11, 7, 3, 2, 5, 10, 9)),
+
+                // Одно блюдо (граничный случай)
+                Arguments.of("WithOneDish",
+                        1,
+                        3,
+                        List.of(1)),
+
+                // Все блюда съедаются по порядку, так как пропуск = 1
+                Arguments.of("WithStepOne",
+                        5,
+                        1,
+                        List.of(1, 2, 3, 4, 5)),
+
+                // Пропуск больше, чем количество блюд
+                Arguments.of("StepGreaterThanDishes",
+                        5,
+                        10,
+                        List.of(5, 1, 2, 3, 4))
         );
     }
 
