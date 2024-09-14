10e3c441f6f0772ae85f6136a88f0b3d1f03436e
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:51:55 +0300

    Update ArrayDequeCreator.java

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,34 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
-import java.util.PriorityQueue;
 import java.util.Queue;
 
-public class ArrayDequeCreator extends PriorityQueue<String> {
+public class ArrayDequeCreator {
+
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        
+        // Первоначально каждый игрок добавляет по две карты в ArrayDeque
+        arrayDeque.add(firstQueue.poll());
+        arrayDeque.add(firstQueue.poll());
+        arrayDeque.add(secondQueue.poll());
+        arrayDeque.add(secondQueue.poll());
+        
+        // Игра продолжается, пока у обоих игроков есть карты
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            // Игрок 1 берет верхнюю карту и добавляет её в конец своей очереди
+            firstQueue.add(arrayDeque.poll());
+            // Игрок 1 добавляет две карты в ArrayDeque
+            arrayDeque.add(firstQueue.poll());
+            arrayDeque.add(firstQueue.poll());
+            
+            // Игрок 2 берет верхнюю карту и добавляет её в конец своей очереди
+            secondQueue.add(arrayDeque.poll());
+            // Игрок 2 добавляет две карты в ArrayDeque
+            arrayDeque.add(secondQueue.poll());
+            arrayDeque.add(secondQueue.poll());
+        }
+        
+        return arrayDeque;
     }
 }
